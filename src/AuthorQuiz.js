e9b1a9519ba3c50aaaf2a9807fcbf5af7b5cffb2
Nathaniel Nunes <manutdkid77@gmail.com>
2019-06-25 12:14:11 +0530

    renamed App.js and refactored references to AuthorQuiz

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -3,10 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css'
 
-class App extends Component {
+class AuthorQuiz extends Component {
   render(){
     return <h4>Hello React with Bootstrap</h4>
   }
 }
 
-export default App;
+export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
 import './index.css';
 
-ReactDOM.render(<App></App>,document.getElementById('root'));
+ReactDOM.render(<AuthorQuiz></AuthorQuiz>,document.getElementById('root'));
