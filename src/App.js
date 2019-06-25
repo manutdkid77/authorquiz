620368c6fc8ebeee6725185249b3b5791ccafdca
Nathaniel Nunes <manutdkid77@gmail.com>
2019-06-25 12:11:51 +0530

    added bootstrap within App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css'
 
 class App extends Component {
-  
+  render(){
+    return <h4>Hello React with Bootstrap</h4>
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-function Greet(props){
-  return <h3>Hello {props.name}, how you doin ?</h3>
-}
-
-ReactDOM.render(<Greet name={'Nathaniel'} />,document.getElementById('root'));
+ReactDOM.render(<App></App>,document.getElementById('root'));
