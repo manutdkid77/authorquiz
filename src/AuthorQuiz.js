0687da63baa6c9bc05f7fb5052c21e0bbd96f64d
Nathaniel Nunes <manutdkid77@gmail.com>
2019-06-26 15:37:30 +0530

    created initial layout of game

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,11 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import './bootstrap.min.css'
 
-function AuthorQuiz(props){
-    var sum = props.num1 + props.num2;
-    return <h4>Hello React with Bootstrap, {props.num1} + {props.num2} is {sum} </h4>
+function Hero() {
+    return (<div className="row">
+        <div className="jumbotron col-10 offset-1">
+            <h1>Author Quiz 📚</h1>
+            <p>Select the author who wrote the 📖</p>
+        </div>
+    </div>);
+}
+
+function Book({title}){
+    return (<p className="answer alert alert-primary">{title}
+    </p>);
+}
+
+function Turn({ author, books }) {
+    return (<div className="row turn" style={{ backgroundColor: "white" }}>
+        <div className="col-4 offset-1">
+            <img src={author.imageUrl} className="authorimage rounded" alt="Author"></img>
+        </div>
+        <div className="col-6">
+            {(books.map(title => <Book title={title} key={title}></Book>))}
+        </div>
+    </div>);
+}
+
+function Continue() {
+    return <div></div>;
+}
+
+function Footer() {
+    return (<div id="footer" className="row">
+        <div className="col-12">
+            <p className="text-muted credit text-center">All images are from <a href="https://commons.wikimedia.org/wiki/Commons:Licensing">Wikimedia Commons</a></p>
+        </div>
+    </div>);
+}
+
+function AuthorQuiz({ turnData }) {
+    return (
+        <div className="container-fluid">
+            <Hero></Hero>
+            <Turn {...turnData}></Turn>
+            <Continue></Continue>
+            <Footer></Footer>
+        </div>);
 }
 
 export default AuthorQuiz;
