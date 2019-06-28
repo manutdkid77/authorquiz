39305e02b10154daacf0ae53da127d880b415f1f
Nathaniel Nunes <manutdkid77@gmail.com>
2019-06-28 15:18:54 +0530

    wrote test cases to check startup and answer colors

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,10 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+//create a dummy state
+const state = {
+  turnData: {
+    author: {
+      name: 'J.K. Rowling',
+      imageUrl: 'images/authors/jkrowling.jpg',
+      imageSource: 'Wikimedia Commons',
+      imageAttribution: 'Daniel Ogren',
+      books: ['Harry Potter and the Sorcerers Stone']
+    },
+    books: ['Hamlet', 'IT', 'Harry Potter and the Sorcerers Stone', 'The Adventures of Huckleberry Finn']
+  },
+  highlight: 'none'
+};
 
 describe("AuthorQuiz", () => {
   test("it renders without crashing", () => {
     let div = document.createElement('div');
-    ReactDOM.render(<AuthorQuiz />,div);
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={() => { }} />, div);
   })
+
+  describe("when no answer has been selected", () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={() => { }}></AuthorQuiz>);
+    });
+
+    test("should have no background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('');
+    });
+  });
+
+  describe("when the wrong answer has been selected", () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...Object.assign({}, state, { highlight: 'wrong' })} onAnswerSelected={() => { }}></AuthorQuiz>)
+    });
+    test("should have a red background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('red');
+    })
+  });
+
+  describe("when the correct answer has been selected", () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...Object.assign({}, state, { highlight: 'correct' })} onAnswerSelected={() => { }}></AuthorQuiz>)
+    });
+    test("should have a green background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('green');
+    })
+  });
 });
